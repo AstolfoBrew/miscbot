aaa00f8afe899b3313ebc022c22fa345f52ad094
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-10-28 02:13:28 +0200

    fix: :sparkles: Change •

--- src/commands/credits.ts
+++ src/commands/credits.ts
@@ -17,18 +17,18 @@ export class Command extends BaseCommand implements IBaseCommand {
         .setColor('#ef9df2')
         .addFields({
           'name': 'Developers',
-          'value': `· Yielding#3961
-· Mokiy#2876`,
+          'value': `• Yielding#3961
+• Mokiy#2876`,
           'inline': true
         },
         {
           'name': 'Testers',
-          'value': `· aga#3014`,
+          'value': `• aga#3014`,
           'inline': true
         }, { // u must have smth providing the license if u use this
           'name': 'License',
-          'value': `· [MIT License](https://github.com/AstolfoBrew/miscbot/blob/master/LICENSE)
-· [Source Code](https://github.com/AstolfoBrew/miscbot)`
+          'value': `• [MIT License](https://github.com/AstolfoBrew/miscbot/blob/master/LICENSE)
+• [Source Code](https://github.com/AstolfoBrew/miscbot)`
         })
         .setFooter({ 'text': 'Made with ❤️, using code, by AstolfoDev', })
         .setTimestamp()]
